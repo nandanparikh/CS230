aab919b660002c1acd8c7de4a18befc9bc6647c9
Prateek Malhotra <prateekdbst@gmail.com>
2019-05-29 14:52:34 -0700

    Update parse-classg.py

--- Code/Callgraph/parse-classg.py
+++ Code/Callgraph/parse-classg.py
@@ -3,22 +3,42 @@ import matplotlib.pyplot as plt
 
 graph_edges = []
 graph_nodes = set()
+graph_nodes_dev = set()
+
+with open('devfiles.txt') as fp:
+    line = fp.readline()
+    while line:
+        graph_nodes_dev.add(line.strip()[2:])
+        line = fp.readline()
+
 with open('classgraph.txt') as fp:
     line = fp.readline()
     while line:
         graph_edges.append(line.strip()[2:].split())
         graph_nodes.add(graph_edges[-1][0])
         graph_nodes.add(graph_edges[-1][1])
+
         line = fp.readline()
+
+fp.close()
+
+color_map = []
+for item in graph_nodes:
+    if item in graph_nodes_dev:
+        color_map.append('green')
+    else:
+        color_map.append('blue')
 
 callgraph = nx.DiGraph()
 callgraph.add_nodes_from(graph_nodes)
 callgraph.add_edges_from(graph_edges)
-print(callgraph.nodes())
-print(callgraph.edges())
+#print(callgraph.nodes())
+#print(callgraph.edges())
+print(graph_nodes_dev)
+print(graph_nodes)
 
 pos = nx.shell_layout(callgraph)
-nx.draw(callgraph, pos, nodes_size = 100, with_labels = True, font_size=10)
+nx.draw(callgraph, pos, node_color = color_map, nodes_size = 100, with_labels = True, font_size=10)
 
 # show graph
 plt.savefig('callgraph_library.png')
